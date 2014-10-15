fb94af86a7319ceac1a2e5d68797600c1f041efc
David Gayerie <dagaydevel@free.fr>
2014-10-15 15:30:32 +0200

    upgrading the application dependencies

--- src/main/java/fr/epsi/i4/bookmark/Main.java
+++ src/main/java/fr/epsi/i4/bookmark/Main.java
@@ -1,21 +1,60 @@
 package fr.epsi.i4.bookmark;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.glassfish.jersey.server.ApplicationHandler;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.simple.SimpleContainerFactory;
+import org.glassfish.jersey.simple.SimpleContainerProvider;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.core.Container;
 
 import fr.epsi.i4.bookmark.web.BookmarkApplication;
 
-public class Main {
+public class Main implements Container {
 
-	public static void main(String[] args) throws Exception {
-		String port = Objects.toString(System.getenv("PORT"), "8080");
-		Logger.getLogger("bookmarks").log(Level.INFO, "Starting web services on port " + port);
-	    SimpleContainerFactory.create(new URI("http://rest-bookmarks.herokuapp.com:" + port), new ApplicationHandler(new BookmarkApplication()));
+	private Container container;
+	
+	public Main() {
+		SimpleContainerProvider provider = new SimpleContainerProvider();
+		container = provider.createContainer(Container.class, new BookmarkApplication());
 	}
 
+	public static void main(String[] args) throws URISyntaxException {
+		String port = Objects.toString(System.getenv("PORT"), "8080");
+        Logger.getLogger("bookmarks").log(Level.INFO, "Starting web services on port " + port);
+		SimpleContainerFactory.create(new URI("http://rest-bookmarks.herokuapp.com:" + 8080), ResourceConfig.forApplication(new BookmarkApplication()));
+	}
+
+	@Override
+	public void handle(Request request, Response response) {
+		if (request.getPath().getPath().equals("/")) {
+			if (request.getMethod().equals("GET")) {
+				response.setContentType("text/html");
+				try(PrintStream printStream = response.getPrintStream()) {
+					printStream.println("Hello");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else {
+				response.setCode(405);
+				try {
+					response.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		else {
+			container.handle(request, response);
+		}
+		
+	}
 }
+
